150878c3bac7827738e2c5b85720aad792ff45ab
codeli4m <applecdos504@gmail.com>
2023-05-05 16:28:15 -0500

    ejemplo basico

---
+++ src/main/java/com/java/ejemplojava/EjemploJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.java.ejemplojava;
+
+/**
+ *
+ * @author liamy
+ */
+public class EjemploJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo escrito por Liam!");
+    }
+}
